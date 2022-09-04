7397b99d1d99bba459c5d6e3f71c1479a74e4510
b72u68 <mdinh@hawk.iit.edu>
2022-09-04 00:05:54 -0500

    Added iitran highlight rules for tree-sitter

---
+++ queries/highlights.scm
@@ -0,0 +1,56 @@
+;highlights.csm
+
+(identifier) @variable
+(character) @constant
+(number) @number
+(comment) @comment
+
+[
+ "STOP"
+ "DO"
+ "END"
+ "IF"
+ "ELSE"
+ "WHILE"
+] @keyword
+
+[
+ "+"
+ "-"
+ "*"
+ "/"
+ "<"
+ "<="
+ ">"
+ ">="
+ "#"
+ "="
+ "<-"
+] @operator
+
+[
+ "NOT"
+ "AND"
+ "OR"
+] @keyword.operator
+
+[
+ "CHARACTER"
+ "LOGICAL"
+ "INTEGER"
+] @type.builtin
+
+[
+ "CHAR"
+ "LG"
+ "INT"
+] @type
+
+[
+ "("
+ ")"
+] @punctuation.bracket
+
+[
+ ","
+] @punctuation.delimiter
